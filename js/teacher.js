06de55ddd9acaed93a38e376194816fab60fe109
sjylim0829-commits <sjylim0829@gmail.com>
2026-08-08 19:21:35 +0900

    Add REDBOOK-GEO-03 Incenter App to Monday LMS live catalog

--- js/teacher.js
+++ js/teacher.js
@@ -27,6 +27,14 @@ const TeacherModule = {
         url: 'https://script.google.com/macros/s/AKfycbzAOh_0HNPAhy25scaJKIykIHywtKGRrNTU6TQKdk0L9berPBqV3xm-qWlUdT_Q3eJOcg/exec',
         desc: 'Redbook 제작: 세 변의 수직이등분선 작도, 외심 O 및 외접원, 외심의 위치 & 각도 성질(∠BOC = 2∠A) 증명 웹 앱 (LMS DB 연동 완료)',
         type: 'gas'
+      },
+      {
+        id: 'REDBOOK-GEO-03',
+        title: '📐 [2학년] 삼각형의 내심 (Incenter) 탐구 (Redbook 웹 앱)',
+        grade: '2학년 전체 (1~8반)',
+        url: 'https://script.google.com/macros/s/AKfycbz1zC7figOuC7FjoEAT4uQ39Kt3fLirKdSOetoIXvavxzqR4WETvwaf875VNBiBQV1N/exec',
+        desc: 'Redbook 제작: 세 내각의 이등분선 작도, 내심 I 및 내접원(r), 세 변까지의 거리(ID=IE=IF) & 각도 성질(∠BIC = 90° + ½∠A) 증명 웹 앱 (LMS DB 연동 완료)',
+        type: 'gas'
       }
     ];
 
